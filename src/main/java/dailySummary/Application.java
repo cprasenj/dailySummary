1d31dbecc8747608f3a5bfe43f86c2dd70a22864
Cprasenj <cprasenj@in-cprasenj.local>
2017-06-11 23:13:40 +0530

    encrypted password

--- src/main/java/dailySummary/Application.java
+++ src/main/java/dailySummary/Application.java
@@ -1,11 +1,11 @@
 package dailySummary;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
--- src/main/java/dailySummary/controller/IndexController.java
+++ src/main/java/dailySummary/controller/IndexController.java
@@ -1,5 +1,6 @@
 package dailySummary.controller;
 
+import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,6 +9,10 @@ public class IndexController {
 
     @RequestMapping("/")
     public String index() {
+        BasicTextEncryptor bte = new BasicTextEncryptor();
+        bte.setPassword("HelloWorld");
+        String encrypted = bte.encrypt("prasenjitchk123@gmail.com");
+        System.out.println("Encrypted = " + encrypted);
         return "dailySummary";
     }
 
