eb388cb5dca8d1b9f67f0681bc899150d79d5c04
Cprasenj <cprasenj@in-cprasenj.local>
2017-06-11 23:15:15 +0530

    removed extra code

--- src/main/java/dailySummary/controller/IndexController.java
+++ src/main/java/dailySummary/controller/IndexController.java
@@ -1,6 +1,5 @@
 package dailySummary.controller;
 
-import org.jasypt.util.text.BasicTextEncryptor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,10 +8,6 @@ public class IndexController {
 
     @RequestMapping("/")
     public String index() {
-        BasicTextEncryptor bte = new BasicTextEncryptor();
-        bte.setPassword("HelloWorld");
-        String encrypted = bte.encrypt("prasenjitchk123@gmail.com");
-        System.out.println("Encrypted = " + encrypted);
         return "dailySummary";
     }
 
