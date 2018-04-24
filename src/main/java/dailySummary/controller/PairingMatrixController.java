0815fb10d1267a74c86ffccf685b81698cf5590d
Abhishek Pradhan <Abhishek_Pradhan@external.mckinsey.com>
2018-04-24 22:31:43 +0530

    Abhsk | Get for both set of pairs for dev1 and dev2

--- src/main/java/dailySummary/controller/PairingMatrixController.java
+++ src/main/java/dailySummary/controller/PairingMatrixController.java
@@ -1,11 +1,14 @@
 package dailySummary.controller;
 
+import dailySummary.contract.Pair;
 import dailySummary.contract.PairMatrixRequest;
 import dailySummary.service.PairingMatrixService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class PairingMatrixController {
@@ -22,7 +25,9 @@ public class PairingMatrixController {
     public ResponseEntity getPairingMatrix(@PathVariable("pair1") String pair1,
                                            @PathVariable("pair2") String pair2
                                            ) {
-        return new ResponseEntity(pairingMatrixService.get(pair1, pair2), HttpStatus.OK);
+        List<Pair> pairs = pairingMatrixService.get(pair1, pair2);
+        pairs.addAll(pairingMatrixService.get(pair2, pair1));
+        return new ResponseEntity(pairs, HttpStatus.OK);
     }
 
     @RequestMapping("/getAll")
