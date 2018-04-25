de42a31b13961660731b38f3ad7e0eead5437f70
Cprasenj <cprasenj@in-cprasenj.local>
2018-04-25 10:24:41 +0530

    Revert "Abhsk | Get for both set of pairs for dev1 and dev2"
    This reverts commit 0815fb10d1267a74c86ffccf685b81698cf5590d.

--- src/main/java/dailySummary/controller/PairingMatrixController.java
+++ src/main/java/dailySummary/controller/PairingMatrixController.java
@@ -1,14 +1,11 @@
 package dailySummary.controller;
 
-import dailySummary.contract.Pair;
 import dailySummary.contract.PairMatrixRequest;
 import dailySummary.service.PairingMatrixService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class PairingMatrixController {
@@ -25,9 +22,7 @@ public class PairingMatrixController {
     public ResponseEntity getPairingMatrix(@PathVariable("pair1") String pair1,
                                            @PathVariable("pair2") String pair2
                                            ) {
-        List<Pair> pairs = pairingMatrixService.get(pair1, pair2);
-        pairs.addAll(pairingMatrixService.get(pair2, pair1));
-        return new ResponseEntity(pairs, HttpStatus.OK);
+        return new ResponseEntity(pairingMatrixService.get(pair1, pair2), HttpStatus.OK);
     }
 
     @RequestMapping("/getAll")
